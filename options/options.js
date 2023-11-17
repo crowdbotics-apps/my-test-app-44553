1895398fb5d8ec9cd46158b65209a24a1f5ce4da
Zulgarain Haider <zulgarain.haider@crowdbotics.com>
2023-11-17 17:59:26 +0500

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,10 +1,11 @@
 export const globalOptions = {
-  name: "my-test-app-44553",
-  url: "https://my-test-app-44553.botics.co",
-  api: "https://my-test-app-44553.botics.co/api/v1",
-  screenOptions: {
-    headerShown: false
-  }
+  "name": "my-test-app-44553",
+  "url": "https://my-test-app-44553.botics.co",
+  "api": "https://my-test-app-44553.botics.co/api/v1",
+  "screenOptions": {
+    "headerShown": false
+  },
+  "initialRoute": "welcome"
 };
 export const modulesOptions = {
   "@modules/app-menu": {
